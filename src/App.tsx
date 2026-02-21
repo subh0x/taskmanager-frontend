b00d62475b3990380ab477c4250c2bf5f5fd6f19
subh0x <sciencesub18@gmail.com>
2026-02-21 20:52:44 +0530

    Setup prettier & ui component Lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  
   return (
     <>
-
+      <Demo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+import { CheckboxCard } from '@chakra-ui/react';
+
+const Demo = () => {
+  return (
+    <>
+      <CheckboxCard.Root maxW="240px">
+        <CheckboxCard.HiddenInput />
+        <CheckboxCard.Control>
+          <CheckboxCard.Content>
+            <CheckboxCard.Label>Next.js</CheckboxCard.Label>
+            <CheckboxCard.Description>Best for apps</CheckboxCard.Description>
+          </CheckboxCard.Content>
+          <CheckboxCard.Indicator />
+        </CheckboxCard.Control>
+      </CheckboxCard.Root>
+    </>
+  );
+};
